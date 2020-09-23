8bac29098d4b4f75c5627b7f4f287168cc76b14a
Patric Puola <patric.puola@gmail.com>
2020-09-23 12:55:52 +0300

    mapbox improvements and bugfixes

--- html/housesitter.py
+++ html/housesitter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, Response
-import sys
+from cairosvg import svg2png
+import sys, math
+from io import BytesIO
 sys.path.insert(1, r'../modules')
 from setup import getConfig, ROOT, getCredentials
 from db import DBCon
@@ -15,10 +17,25 @@ def getNavLinks():
         links.append({'href':'/'+site, 'text':site.capitalize()})
     return links
 
-def getListings(count = 20, offset = 0):
+def getListings(count = 20, offset = 0, getCoordinates = False):
+    listings = []
     with DBCon.get().cursor() as cursor:
-        cursor.execute("SELECT id, url, site, housing_type, street_address, zip, city, suburb, price, country, agency, layout, living_space_m2, total_space_m2, build_year FROM listings ORDER BY date_updated DESC LIMIT %d OFFSET %d" % (count, offset))
-        listings = cursor.fetchall()
+        cursor.execute("SELECT id, url, site, housing_type, description,  street_address, zip, city, suburb, price, country, agency, layout, living_space_m2, total_space_m2, build_year FROM listings ORDER BY date_updated DESC LIMIT %d OFFSET %d" % (count, offset))
+        with DBCon.get().cursor() as geo_cursor:
+            while True:
+                listing = cursor.fetchone()
+                if listing == None:
+                    break
+                listing["lng"] = None
+                listing["lat"] = None
+                if getCoordinates is True:
+                    full_address = listing["street_address"]+", "+str(listing["zip"])+" "+listing["city"]
+                    geo_cursor.execute("SELECT lat, lng FROM geocodes WHERE query = '{}' LIMIT 1".format(full_address))
+                    coords = geo_cursor.fetchone()
+                    if coords:
+                        listing["lng"] = coords['lng']
+                        listing["lat"] = coords['lat']
+                listings.append(listing)
     return listings
 
 def getImageIds(listing_id: int, limit = None):
@@ -55,16 +72,23 @@ def getStats():
     return stats
 
 def getZoom(lng_range, lat_range):
+    def getBaseLog(x, y):
+        return math.log(y) / math.log(x)
     LNG_MAX = 80
     LNG_MIN = -180
     LAT_MAX = 90
     LAT_MIN = -90
     range_percentage_lng = lng_range / (abs(LNG_MIN)+LNG_MAX)
     range_percentage_lat = lat_range / (abs(LAT_MIN)+LAT_MAX)
-    # visible_prct = 100% / 1 + zoom_unit
-    return 9
+    zoom = abs(getBaseLog(2, range_percentage_lat))
+    return zoom
 
-
+def getListingMarkers():
+    markers = None
+    with DBCon.get().cursor() as cursor:
+        cursor.execute("SELECT lng, lat FROM geocodes")
+        markers = cursor.fetchall()
+    return markers
 
 def getMapStartingPoint():
     starting_point = {'lng':None, 'lat':None, 'zoom': 9}
@@ -93,7 +117,6 @@ def listings(detail=None):
         for listing in listings:
             listing['images'] = []
             image_ids = getImageIds(listing['id'])
-            print(image_ids)
             for image_id in image_ids:
                 listing['images'].append({'url':'/image/'+str(image_id), 'id':image_id})
            
@@ -107,11 +130,28 @@ def image(id = None):
         img_data = img_file.read()
     return Response(response=img_data, headers={'Content-type':mime_type}, mimetype=mime_type)
 
-
 @app.route('/map')
 def map():
     access_token = getCredentials()['mapbox']['access_token']
-    return render_template('map.html', nav=getNavLinks(), access_token=access_token, starting_point=getMapStartingPoint())
+    listings = getListings(20, 0, True)
+    for listing in listings:
+        image_ids = getImageIds(listing['id'], 1)
+        listing['thumbnail_url'] = None
+        if len(image_ids) > 0:
+            listing['thumbnail_url'] = '/image/'+str(image_ids[0])
+    return render_template('map.html', nav=getNavLinks(), access_token=access_token, starting_point=getMapStartingPoint(), listings=listings)
+
+@app.route('/asset/map_marker/<int:intensity>')
+def asset(intensity = 0):
+    red = 255 if intensity >= 50 else (intensity/50) * 255
+    green = 255 if intensity <= 50 else 255 - ((intensity-50)/50) * 255
+    asset = ROOT / "html/static/marker.svg"
+    with open(asset, 'r') as asset_file:
+        asset_data = asset_file.read()
+    asset_data = asset_data.replace('{{ marker_color }}', 'rgb({},{},0)'.format(red,green)).replace('{{ dot_color }}', 'white')
+    png = BytesIO()
+    svg2png(bytestring=asset_data, write_to=png)
+    return Response(response=png.getvalue(), headers={'Content-type':'image/png'}, mimetype='image/png')
 
 if __name__ == '__main__':
     app.debug = True
